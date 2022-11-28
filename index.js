9d435b9052236d0e67b82ac200a11bfb57c6805d
Ilya <ilya.shuvatov1@gmail.com>
2022-11-28 12:09:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const footballer = {
+    fullName: 'Cristiano Ronaldo',
+    attack: () => {
+        console.log(`${this.fullName} сейчас с мячом и начинает атаку!`);
+    },
+    scoreGoal(sound) {
+        console.log(`${this.fullName} забил гол! Вот это да!`);
+        this.celebrate(sound);
+    },
+    celebrate(sound) {
+        console.log(sound);
+    },
+    goToSubstitution: function(newPlayer) {
+        console.log(`${this.fullName} уходит на замену. На поле выходит ${newPlayer}`);
+    }
+};
+
+ const  attack = function () {
+    console.log ((`${this.fullName} сейчас с мячом и начинает атаку!`))
+}
+attack.bind(footballer)()
+
+const score = footballer.scoreGoal;
+ score.call(footballer, 'сииии')
+
+
+const substitute = footballer.goToSubstitution;
+ substitute.apply(footballer, ['Paulo Dibala'])
+
+
+
+
